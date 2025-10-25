b9c49e14e780d6b19bf92833151dd795642d39a1
JacobBatson <jacobbatson123@aol.com>
2025-10-24 23:09:47 -0400

    added a submitted screen

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,16 @@ import Welcome from './welcome';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Survey from "./survey";
+import Submitted from "./submitted";
 
 const Stack = createStackNavigator();
 
 export default function Index() {
   return (
       <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={Welcome} options={{title: "Welcome"}}/>
+        <Stack.Screen name="Welcome" component={Welcome} options={{title: "Welcome", headerLeft: () => null,}}/>
         <Stack.Screen name="Survey" component={Survey} options={{title: "Survey"}}/>
+        <Stack.Screen name="Submitted" component={Submitted} options={{title: "Submitted", headerLeft: () => null}}/>
       </Stack.Navigator>
   )
 }
