fd874af0b3364da9eb4f1d7d0a7ff7fd50e6f1b4
Wilson Chen <wilson.chen@quinnipiac.edu>
2025-10-24 20:19:22 -0400

    Deleting some changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Welcome from './welcome';
 import Survey from './survey';
-import CenteredSliders from "./survey2";
 
 export default function Index() {
   return <Survey/>;
