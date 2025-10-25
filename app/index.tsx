982d7a97d1d7b0902eada6f26751db0e33bb76dd
Wilson Chen <wilson.chen@quinnipiac.edu>
2025-10-24 20:15:47 -0400

    Added three separate questions and their own sliders that are independent of the others.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Welcome from './welcome';
+import Survey from './survey';
+import CenteredSliders from "./survey2";
 
 export default function Index() {
-  return <Welcome/>;
+  return <Survey/>;
 }
