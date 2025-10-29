0f4804bd4bc71142d14e4f2a03a0d3f47b5c9f88
Wilson Chen <wilson.chen@quinnipiac.edu>
2025-10-29 00:54:33 -0400

    Added a functionality where the user's entries are saved into the local drive with the current time of submission. Buttons to export and clear the data are located on the welcome screen.

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,11 +1,54 @@
-import { View , Text, TouchableOpacity, StyleSheet} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View , Text, TouchableOpacity, StyleSheet, Alert} from "react-native";
+import * as Sharing from "expo-sharing";
+import { File, Paths } from "expo-file-system";
+
+
 
 export default function Welcome({navigation}: {navigation: any}) {
+    const STORAGE_KEY = "testDataCollection";
+
+    const handleExportData = async (): Promise<void> => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("testDataCollection");
+        if (!jsonValue) {
+          Alert.alert("No data to export.");
+          return;
+        }
+
+        const file = new File(Paths.document, `testData_${Date.now()}.json`);
+        await file.write(jsonValue);
+
+        await Sharing.shareAsync(file.uri);
+        console.log("Exported to file:", file.uri);
+      } catch (err) {
+        console.error("Error exporting data:", err);
+      }
+    };
+
+    const handleClearData = async (): Promise<void> => {
+      try {
+        await AsyncStorage.removeItem("testDataCollection");
+        Alert.alert("Cleared", "All saved test data has been removed.");
+      } catch (err) {
+        console.error("Error clearing data:", err);
+        Alert.alert("❌ Error", "Failed to clear data.");
+      }
+    };
+    
   return (
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Stress Form</Text>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Survey")}>
-        <Text style={styles.buttonText}>Go to survey Survey</Text>
+        <Text style={styles.buttonText}>Go to Survey</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={handleExportData}>
+        <Text style={styles.buttonText}>Export Data</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={handleClearData}>
+        <Text style={styles.buttonText}>Clear Data</Text>
       </TouchableOpacity>
     </View>
   );
