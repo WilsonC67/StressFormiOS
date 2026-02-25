833c16b92d5aa28e7567e868ba42c045824ed854
ThisIsHP64 <thisishp64@gmail.com>
2026-02-25 09:57:32 -0500

    attempt name fix

--- ios/StressFormReport/TotalActivityReport.swift
+++ ios/StressFormReport/TotalActivityReport.swift
@@ -2,7 +2,7 @@
 //  TotalActivityReport.swift
 //  StressFormReport
 //
-//  Created by Hunter Pageau on 12/29/25.
+//  Created by Hunter Pageau on 2/25/26.
 //
 
 import DeviceActivity
@@ -10,14 +10,22 @@ import ExtensionKit
 import SwiftUI
 
 extension DeviceActivityReport.Context {
+    // If your app initializes a DeviceActivityReport with this context, then the system will use
+    // your extension's corresponding DeviceActivityReportScene to render the contents of the
+    // report.
     static let totalActivity = Self("Total Activity")
 }
 
 struct TotalActivityReport: DeviceActivityReportScene {
+    // Define which context your scene will represent.
     let context: DeviceActivityReport.Context = .totalActivity
+    
+    // Define the custom configuration and the resulting view for this report.
     let content: (String) -> TotalActivityView
     
     func makeConfiguration(representing data: DeviceActivityResults<DeviceActivityData>) async -> String {
+        // Reformat the data into a configuration that can be used to create
+        // the report's view.
         let formatter = DateComponentsFormatter()
         formatter.allowedUnits = [.day, .hour, .minute, .second]
         formatter.unitsStyle = .abbreviated
